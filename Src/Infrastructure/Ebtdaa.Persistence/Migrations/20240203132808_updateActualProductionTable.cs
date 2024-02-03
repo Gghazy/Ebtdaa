64dafdafb16437b924c87eb0fad529ec95991cf4
Gghazy <a.ghazy@automationxgroup.com>
2024-02-03 15:28:42 +0200

    update actual production

---
+++ Src/Infrastructure/Ebtdaa.Persistence/Migrations/20240203132808_updateActualProductionTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ebtdaa.Persistence.Migrations
+{
+    public partial class updateActualProductionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Month",
+                table: "ActualProductionAndCapacities");
+
+            migrationBuilder.RenameColumn(
+                name: "ReasoneForIncreaseCapacity",
+                table: "ActualProductionAndCapacities",
+                newName: "MonthId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MonthId",
+                table: "ActualProductionAndCapacities",
+                newName: "ReasoneForIncreaseCapacity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Month",
+                table: "ActualProductionAndCapacities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
