e9ead2355e9ad6b12e4bb4ee0043a450782716df
Doha-Khair <Doha@DESKTOP-3R1LKG9>
2024-02-26 06:32:03 +0200

    update

--- Src/Infrastructure/Ebtdaa.Persistence/Migrations/20240226041545_customItemrawMaterial.Designer.cs
+++ Src/Infrastructure/Ebtdaa.Persistence/Migrations/20240226041545_customItemrawMaterial.Designer.cs
@@ -4,6 +4,7 @@ using Ebtdaa.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ebtdaa.Persistence.Migrations
 {
     [DbContext(typeof(EbtdaaDbContext))]
-    partial class EbtdaaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240226041545_customItemrawMaterial")]
+    partial class customItemrawMaterial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Src/Infrastructure/Ebtdaa.Persistence/Migrations/20240226041545_customItemrawMaterial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ebtdaa.Persistence.Migrations
+{
+    public partial class customItemrawMaterial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RawMaterials_CustomItemRawMaterials_CustomItemRawMaterialId",
+                table: "RawMaterials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RawMaterials_CustomItemRawMaterialId",
+                table: "RawMaterials");
+
+            migrationBuilder.DropColumn(
+                name: "CustomItemRawMaterialId",
+                table: "RawMaterials");
+
+            migrationBuilder.DropColumn(
+                name: "ItemNumber",
+                table: "RawMaterials");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomItemRawMaterialId",
+                table: "RawMaterials",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemNumber",
+                table: "RawMaterials",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RawMaterials_CustomItemRawMaterialId",
+                table: "RawMaterials",
+                column: "CustomItemRawMaterialId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RawMaterials_CustomItemRawMaterials_CustomItemRawMaterialId",
+                table: "RawMaterials",
+                column: "CustomItemRawMaterialId",
+                principalTable: "CustomItemRawMaterials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
