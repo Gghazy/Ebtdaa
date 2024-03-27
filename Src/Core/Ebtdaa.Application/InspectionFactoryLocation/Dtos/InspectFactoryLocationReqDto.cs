45366b5c7503aff3faaaa8f1bf3e35c3cc601e09
Walla Attif <w.atif@automationxgroup.com>
2024-03-27 23:52:15 +0200

    inspect factoryLocation

---
+++ Src/Core/Ebtdaa.Application/InspectionFactoryLocation/Dtos/InspectFactoryLocationReqDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebtdaa.Application.InspectionFactoryLocation.Dtos
+{
+    public class InspectFactoryLocationReqDto
+    {
+        public int Id { get; set; }
+        public int FactoryId { get; set; }
+        public int PeriodId { get; set; }
+        public int FactoryEntityId { get; set; }
+        public int CityId { get; set; }
+        public int IndustrialAreaId { get; set; }
+        public string WebSite { get; set; }
+        public bool IsFactoryEntityCorrect { get; set; }
+        public bool IsCityCorrect { get; set; }
+        public bool IsIndustrialAreaCorrect { get; set; }
+        public int? NewFactoryEntityId { get; set; }
+        public int? NewCityId { get; set; }
+        public int? NewIndustrialAreaId { get; set; }
+        public string? NewWebSite { get; set; }
+
+    }
+}
