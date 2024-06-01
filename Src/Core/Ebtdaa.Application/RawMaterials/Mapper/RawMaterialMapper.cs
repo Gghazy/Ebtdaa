f44872a8818b4debabb6cdd2214a556b84e712c3
Doha-Khair <Doha@DESKTOP-3R1LKG9>
2024-06-01 14:41:58 +0300

    ..

--- Src/Core/Ebtdaa.Application/RawMaterials/Mapper/RawMaterialMapper.cs
+++ Src/Core/Ebtdaa.Application/RawMaterials/Mapper/RawMaterialMapper.cs
@@ -13,7 +13,7 @@ namespace Ebtdaa.Application.RawMaterials.Mapper
             {
             CreateMap<RawMaterialRequestDto, RawMaterial>();
             CreateMap<RawMaterial, RawMaterialResultDto>()
-             .ForMember(dest => dest.FactoryProductId, opt => opt.MapFrom(src => src.ProductRawMaterials.Select(prm => prm.ProductId)))
+             .ForMember(dest => dest.FactoryProductId, opt => opt.MapFrom(src => src.ProductRawMaterials.Select(prm => prm.ProductId)));
           
 
 
