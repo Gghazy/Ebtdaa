9fb75694fdf8e0eba526b494e2da630fca0d8d3d
Walla Attif <w.atif@automationxgroup.com>
2024-06-04 17:03:45 +0300

    fix screenstatus for actual production

--- Src/Core/Ebtdaa.Application/ScreenUpdateStatus/Handlers/ScreenStatusService.cs
+++ Src/Core/Ebtdaa.Application/ScreenUpdateStatus/Handlers/ScreenStatusService.cs
@@ -267,7 +267,7 @@ namespace Ebtdaa.Application.ScreenUpdateStatus.Handlers
 
         private bool IsProductiveScreenValid(List<int> differenceList, List<ActualProductionAndCapacity> result)
         {
-            return !(differenceList.Any() || result.Any(x => x.ActualProduction == null) || result.Count == 0);
+            return !(differenceList.Any() || result.Any(x => x.ActualProduction == null || x.ActualProduction == 0) || result.Count == 0);
         }
 
         private bool AreAttachmentsComplete(int? factoryId, int periodId)
