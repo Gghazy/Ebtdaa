34f16ba6c43cc55dc2de754c1e9a40c3d639b6a6
lobaba <lobaba.eltayeb@gmail.com>
2024-07-22 10:52:00 +0300

    loginbynafath

---
+++ Src/Presentation/Ebtdaa.WebApi/Middlewares/MyHttpClient.cs
@@ -0,0 +1,26 @@
+﻿namespace Ebtdaa.WebApi.Middlewares
+{
+    public class MyHttpClient
+    {
+        private readonly HttpClient _client;
+
+        public MyHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> GetDataAsync(string url)
+        {
+            HttpResponseMessage response = await _client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                // Handle error response
+                return $"Error: {response.StatusCode}";
+            }
+        }
+    }
+}
