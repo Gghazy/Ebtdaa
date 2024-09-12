085dbc5a50dd88b7439ba66e1dde345b7c99b332
Walla Attif <w.atif@automationxgroup.com>
2024-09-12 17:59:43 +0300

    distinct products

--- Src/Core/Ebtdaa.Application/ProductsData/Handlers/ProductDataService.cs
+++ Src/Core/Ebtdaa.Application/ProductsData/Handlers/ProductDataService.cs
@@ -59,6 +59,7 @@ namespace Ebtdaa.Application.ProductsData.Handlers
                     .Include(x=>x.ProductPeriodActives)
                     //.Include(x=>x.FactoryProducts)
                    .Where(r => r.CR == getCR.CommercialRegister || productActive.Contains(r.Id) || productInfactory.Contains(r.Id))
+                   .Distinct()
                     //.Where(x=>x.ProductPeriodActives.Any(r=>r.FactoryId==search.FactoryId&&r.PeriodId==r.PeriodId))
                     .Join(_dbContext.MappingProducts, a => a.ItemNumber, b => b.Hs10Code, (a, b) =>
                     new ProductResultDto
